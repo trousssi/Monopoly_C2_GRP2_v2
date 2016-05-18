41a14dccd7fddbed3a7114ad885833369e4bfb0c
trousssi <trousssi@pc-dg-027-06>
2016-05-18 17:41:20 +0200

    package

--- Monopoly/src/Jeu/Monopoly.java
+++ Monopoly/src/Jeu/Monopoly.java
@@ -1,22 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package monopoly;
+package Jeu;
 
-/**
- *
- * @author trousssi
- */
-public class Monopoly {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.print("BITE2");
-        System.out.println("Simon");              
-    }
-    
+public class Controleur {
+	public CreerPlateau(String dataFilename){
+		buildGamePlateau(dataFilename);
+	}
+	
+	private void buildGamePlateau(String dataFilename)
+	{
+		try{
+			ArrayList<String[]> data = readDataFile(dataFilename, ",");
+			
+			//TODO: create cases instead of displaying
+			for(int i=0; i<data.size(); ++i){
+				String caseType = data.get(i)[0];
+				if(caseType.compareTo("P") == 0){
+					System.out.println("Propriété :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("G") == 0){
+					System.out.println("Gare :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("C") == 0){
+					System.out.println("Compagnie :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else if(caseType.compareTo("AU") == 0){
+					System.out.println("Case Autre :\t" + data.get(i)[2] + "\t@ case " + data.get(i)[1]);
+				}
+				else
+					System.err.println("[buildGamePleateau()] : Invalid Data type");
+			}
+			
+		} 
+		catch(FileNotFoundException e){
+			System.err.println("[buildGamePlateau()] : File is not found!");
+		}
+		catch(IOException e){
+			System.err.println("[buildGamePlateau()] : Error while reading file!");
+		}
+	}
+	
+	private ArrayList<String[]> readDataFile(String filename, String token) throws FileNotFoundException, IOException
+	{
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		
+		BufferedReader reader  = new BufferedReader(new FileReader(filename));
+		String line = null;
+		while((line = reader.readLine()) != null){
+			data.add(line.split(token));
+		}
+		reader.close();
+		
+		return data;
+	}
 }
+
