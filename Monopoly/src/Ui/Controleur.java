71a9305564d2bd1888bad43e7102ff56ebe0398c
trousssi <simon.troussier@outlook.fr>
2016-05-21 18:11:52 +0200

    Add files via upload

---
+++ Monopoly/src/Ui/Controleur.java
@@ -0,0 +1,18 @@
+package Ui;
+
+import Jeu.Monopoly;
+import Jeu.Joueur;
+import Jeu.Carreau;
+
+public class Controleur {
+	public IHM ihm;
+	public Monopoly monopoly;
+
+	public void jouerUnCoup(Joueur aJ) {
+		throw new UnsupportedOperationException();
+	}
+
+	private Carreau lancerDésAvancer(Joueur aJ) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ Monopoly/src/Ui/IHM.java
@@ -0,0 +1,12 @@
+package Ui;
+
+import Jeu.Joueur;
+import Jeu.Carreau;
+
+public class IHM {
+	public Controleur controleur;
+
+	public void messageJoueurAvance(Joueur aJoueur, int aSommeDes, Carreau aCarreau) {
+		throw new UnsupportedOperationException();
+	}
+}
