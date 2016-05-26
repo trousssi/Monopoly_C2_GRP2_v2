e3f485bc4d324437c3963beed710210a8f98d02c
Mohameth <fallm@etu.upmf-grenoble.fr>
2016-05-26 11:02:35 +0200

    Update AutreCarreau.java

--- Monopoly/src/Jeu/AutreCarreau.java
+++ Monopoly/src/Jeu/AutreCarreau.java
@@ -6,4 +6,9 @@ public class AutreCarreau extends Carreau {
         super(numero, nomCarreau);
     }
     
+    @Override
+    public Resultat action(Joueur aj) {
+        Resultat res = new Resultat(this.getNumero(), this.getNomCarreau());
+        return res;
+    }
 }
