9d5982a48497dfde52677b182af6050206d9c1dd
fallm <fallm@pc-dg-039-02>
2016-06-07 12:39:54 +0200

    Merge origin/master
    Conflicts:
    	Monopoly/src/Jeu/Monopoly.java

--- Monopoly/src/Jeu/ProprieteAConstruire.java
+++ Monopoly/src/Jeu/ProprieteAConstruire.java
@@ -15,12 +15,9 @@ public class ProprieteAConstruire extends Propriete {
         //Calcule le loyer à payer lorsqu'on tombe sur une propriete à construire appartenant à un joueur.
 	@Override
 	protected Resultat calculLoyer(int sommeDes) { // sommeDes n'est pas utilisé.
-            Joueur jproprio = this.getProprietaire(); // Proprietaire de la ProprieteAConstruire
-            int nbPropGroupe = this.groupe.getNbPropriete(); // Nombre de propriété dans le groupe de couleur de la ProprieteAConstruire
-            int nbPropGroupePossede = jproprio.getNbPropriete(this.groupe); // Nombre de propriété de ce groupe possèdées par le propriétaire de la ProprieteAConstruire
-
             Resultat res = new Resultat();
-            if (nbPropGroupe == nbPropGroupePossede) { // Si le joueur possède toutes les propriétés du groupe de couleur de la ProprieteAConstruire
+            Joueur jProprio = this.getProprietaire();       
+            if (this.possedeToutesPropGroupe(jProprio)) { // Si le proprietaire possède toutes les propriétés du groupe de couleur de la ProprieteAConstruire
                 res.setLoyerPropriete(this.getLoyer()*2); // Alors on double le loyer nu
             }
             else { 
@@ -29,9 +26,18 @@ public class ProprieteAConstruire extends Propriete {
             return res;
 	}
         
-  @Override
-    public void setProprietaire(Joueur j) {
-        this.proprietaire = j;
-        j.addProprieteAConstruire(this);
-    }
+        public boolean possedeToutesPropGroupe(Joueur j) {
+            if (j != this.getProprietaire()) return false; //Un joueur non proprietaire de la carte ne possede aucun propriete du dit groupe
+            
+            int nbPropGroupe = this.groupe.getNbPropriete(); // Nombre de propriété dans le groupe de couleur de la ProprieteAConstruire
+            int nbPropGroupePossede = j.getNbPropriete(this.groupe); // Nombre de propriété de ce groupe possèdées par le propriétaire de la ProprieteAConstruire
+            
+            return (nbPropGroupe == nbPropGroupePossede);// Si le joueur possède toutes les propriétés du groupe de couleur de la ProprieteAConstruire on renvoie vrai
+        }
+        
+        @Override
+        public void setProprietaire(Joueur j) {
+            this.proprietaire = j;
+            j.addProprieteAConstruire(this);
+        }
 }
